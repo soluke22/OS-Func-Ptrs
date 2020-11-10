0054ecfdd3e716d7bfb61a6a6fec61ade853eab8
Solomon Lucas-Thornton <solomonlucasthornton@gmail.com>
2020-11-10 17:56:45 +0000

    Files updated.

--- task2/task2.c
+++ task2/task2.c
@@ -22,11 +22,11 @@ int main (void)//use the function array, never explicity call add/multiply/subtr
   p[3] = divide;
   
   printf("'a':%d | 'b':%d \n", x, y);
-  printf("Specify the operation to perform(0 : add | 1 : subtract | 2 : Multiply | 3 : divide | 4+ : exit):");
+  printf("Specify the operation to perform(0 : add | 1 : subtract | 2 : Multiply | 3 : divide | 4+ : exit):\n");
   scanf("%d", &u_input);
   while(u_input < 4){
     printf("Output: %d \n", (*p[u_input])(x,y));
-    printf("Specify the operation to perform(0 : add | 1 : subtract | 2 : Multiply | 3 : divide | 4+ : exit):");
+    printf("Specify the operation to perform(0 : add | 1 : subtract | 2 : Multiply | 3 : divide | 4+ : exit):\n");
     scanf("%d", &u_input);
   }
   printf("Bye bye! \n");
